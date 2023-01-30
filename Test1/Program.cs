5cb86ede99658f26cd06d0ad095bbf7a045a627c
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-30 10:04:52 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,35 @@
+﻿int lines = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[lines, columns];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+ 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
