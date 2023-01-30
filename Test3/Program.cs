fb331c13fe9366a45f610d40a16cc6430a10894d
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-30 10:07:59 +0300

    Test3

---
+++ Test3/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,] FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return matrix;
+}
+
+void NewMatrix(int[,] array, int rows, int columns)
+{
+
+    for (int j = 0; j < columns; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            sum += array[i, j];
+
+        }
+        Console.WriteLine($"Среднее арифметическое столбце {j} = {Math.Round(sum / rows, 2)}");
+    }
+
+}
+
+int m = new Random().Next(3, 7);
+int n = new Random().Next(3, 7);
+int[,] matrix = new int[m, n];
+FillArrayWithRandom(matrix, m, n);
+NewMatrix(matrix, m, n);
